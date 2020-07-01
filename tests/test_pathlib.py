b5250d92021e2ed9b87e0d48e3df64f8feb705be
bitranox <rnowotny1966@gmail.com>
2020-07-01 21:26:29 +0200

    refractor

--- tests/test_pathlib.py
+++ tests/test_pathlib.py
@@ -6,3 +6,5 @@ def test_pathlib():
     >>> test_pathlib()
     """
     pathlib.Path('__not_existing__').unlink(missing_ok=True)
+    pathlib.Path('.').glob('**/*')
+    pathlib.Path('.').rglob('**/*')
