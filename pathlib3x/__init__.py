1f9b6d7eb121b947594541a286adaabab55f6128
bitranox <rnowotny1966@gmail.com>
2020-07-05 23:38:15 +0200

    refractor

--- pathlib3x/__init__.py
+++ pathlib3x/__init__.py
@@ -1,2 +1,10 @@
 __all__ = ["PurePath", "PurePosixPath", "PureWindowsPath", "Path", "PosixPath", "WindowsPath"]
 from .pathlib3x import *
+
+from . import __init__conf__
+__title__ = __init__conf__.title
+__version__ = __init__conf__.version
+__name__ = __init__conf__.name
+__url__ = __init__conf__.url
+__author__ = __init__conf__.author
+__author_email__ = __init__conf__.author_email
