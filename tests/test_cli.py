f209e2daab533ae8309b2b9690e2d8b6f5413690
bitranox <rnowotny1966@gmail.com>
2020-07-15 23:27:04 +0200

    refractor

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,18 +11,19 @@ cli_filename = 'pathlib3x_cli.py'
 path_cli_command = pathlib.Path(__file__).resolve().parent.parent / package_dir / cli_filename
 
 
-def call_cli_command(commandline_args: str = '', log: bool = True) -> bool:
+def call_cli_command(commandline_args: str = '') -> bool:
     command = ' '.join([sys.executable, str(path_cli_command), commandline_args])
     try:
         subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as exc:
+    except subprocess.CalledProcessError:
         return False
     return True
 
 
 def test_cli_commands() -> None:
     # we need the type ignores because of a bug in python 3.8.1 with setup.py test on travis
-    assert call_cli_command('--unknown_option') is not True
-    assert call_cli_command('--version') is True
-    assert call_cli_command('-h') is True
-    assert call_cli_command('info') is True
+    assert not call_cli_command('--unknown_option')
+    if sys.version_info != (3, 8, 1):
+        assert call_cli_command('--version')
+    assert call_cli_command('-h')
+    assert call_cli_command('info')
