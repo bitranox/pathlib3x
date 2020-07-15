c9406896d499e586a9aeac0c17c6c40fa750da97
bitranox <rnowotny1966@gmail.com>
2020-07-16 00:00:45 +0200

    refractor

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,6 +23,7 @@ def call_cli_command(commandline_args: str = '') -> bool:
 def test_cli_commands() -> None:
     # we need the type ignores because of a bug in python 3.8.1 with setup.py test on travis
     assert not call_cli_command('--unknown_option')
-    # assert call_cli_command('--version')
-    assert call_cli_command('-h')
+    if sys.version_info < (3, 8, 1) or sys.version_info >= (3, 8, 2):
+        assert call_cli_command('--version')
+        assert call_cli_command('-h')
     assert call_cli_command('info')
