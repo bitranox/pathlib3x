6b6dd522f476cac56d04a19bcfd1ea9f47723efc
bitranox <rnowotny1966@gmail.com>
2020-08-01 19:31:46 +0200

    refractor

--- .docs/README_template.rst
+++ .docs/README_template.rst
@@ -1,7 +1,8 @@
 pathlib3x
 =========
 
-Version v1.3.7 as of 2020-08-01, see `Changelog`_.
+
+Version v1.3.7 as of 2020-08-01 see `Changelog`_
 
 
 .. include:: ./badges.rst
--- setup.py
+++ setup.py
@@ -13,12 +13,8 @@ from setuptools import find_packages
 
 
 def is_travis_deploy() -> bool:
-    if 'DEPLOY' not in os.environ:
-        return False
-    if os.environ['DEPLOY'].lower() == 'true' and is_tagged_commit():
-        return True
-    else:
-        return False
+    if os.getenv('DEPLOY_SDIST', '') or os.getenv('DEPLOY_WHEEL', ''):
+        return is_tagged_commit()
 
 
 def is_tagged_commit() -> bool:
