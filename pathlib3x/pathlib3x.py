2c5e307760f2ae5ccca94bdc972a5fc92b714fba
bitranox <rnowotny1966@gmail.com>
2022-06-03 17:43:10 +0200

    upgrade github actions to checkup@v3 and setup-python@v3

--- pathlib3x/pathlib3x.py
+++ pathlib3x/pathlib3x.py
@@ -19,6 +19,7 @@ import warnings
 from _collections_abc import Sequence
 from errno import ENOENT, ENOTDIR, EBADF, ELOOP
 from operator import attrgetter
+
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
 from urllib.parse import quote_from_bytes as urlquote_from_bytes
 
@@ -27,7 +28,7 @@ if sys.version_info < (3, 6):
     raise ImportError("pathlib3x is only compatible with Python 3.6 or newer")
 
 if sys.version_info < (3, 10):
-    import pathlib_original
+    from . import pathlib_original
 # /bitranox addon
 
 __all__ = [
